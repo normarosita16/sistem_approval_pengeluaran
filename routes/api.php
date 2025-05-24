298ea9ed373bc6a641290b735a14d0b0a0ad9e55
Norma Rosita Damayanthi <norma.rosita@elnusa.co.id>
2025-05-24 13:43:30 +0700

    add comment on route

--- routes/api.php
+++ routes/api.php
@@ -2,10 +2,16 @@ use App\Http\Controllers\Api\ApproverController;
 use App\Http\Controllers\Api\ApprovalStageController;
 use App\Http\Controllers\Api\ExpenseController;
 
+// untuk menambahkan approver
 Route::post('/approvers', [ApproverController::class, 'store']);
+// untuk menambahkan tahapan approval
 Route::post('/approval-stages', [ApprovalStageController::class, 'store']);
+// untuk mengubah tahapan approval.
 Route::put('/approval-stages/{id}', [ApprovalStageController::class, 'update']);
+// untuk menambahkan pengeluaran
 Route::post('/expense', [ExpenseController::class, 'store']);
+// untuk approver bisa approve sesuai urutan
 Route::put('/expense/{id}/approve', [ExpenseController::class, 'approve']);
+// untuk melihat detail pengeluaran dan seluruh tahapan approval-nya.
 Route::get('/expense/{id}', [ExpenseController::class, 'show']);
 
